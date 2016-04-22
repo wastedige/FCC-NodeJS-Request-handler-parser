b98f6bf330b55c530c764877dccb6977814415d5
Shaahin <shaahin@gmail.com>
2016-04-21 23:54:41 -0500

    Working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var os = require('os');
+var ip = require('ip');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,11 +12,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var addr;
+
+  response.send({
+    Address: ip.address(),
+    OS:  os.type() + " " + os.release() + " " + os.arch(),
+    language: request.headers['accept-language'].split(",")[0]
+  });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
